833a95bbecd487c3f900221b48ae26ce0d956d9b
Akash <akashrajawat.ar@google.com>
2024-04-17 22:46:49 +0530

    Create Element and Grid Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Card from './Card';
 
 function App() {
+  const elements = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"];
   return (
     <div className="App">
-      
+      {elements.map((item, index) => (<Card key={index}>{item}</Card>))}
     </div>
   );
 }
